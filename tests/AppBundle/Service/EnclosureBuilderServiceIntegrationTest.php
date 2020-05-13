83922234f15e9665781a16651b13939e1da7e7e9
Nelson Moller <nmoller.c@gmail.com>
2020-05-13 16:04:52 -0400

    Unstable integration test...caution.

--- tests/AppBundle/Service/EnclosureBuilderServiceIntegrationTest.php
+++ tests/AppBundle/Service/EnclosureBuilderServiceIntegrationTest.php
@@ -3,7 +3,10 @@
 
 namespace Tests\AppBundle\Service;
 
+use AppBundle\Entity\Dinosaur;
+use AppBundle\Entity\Security;
 use AppBundle\Service\EnclosureBuilderService;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class EnclosureBuilderServiceIntegrationTest extends KernelTestCase
@@ -11,7 +14,31 @@ class EnclosureBuilderServiceIntegrationTest extends KernelTestCase
     public function testItBuildsEnclosureWithDefaultSpecs()
     {
         self::bootKernel();
+        /** @var EnclosureBuilderService $enclosureBuilderService */
         $enclosureBuilderService = self::$kernel->getContainer()
             ->get('test.' . EnclosureBuilderService::class);
+
+        $enclosureBuilderService->buildEnclosure();
+
+        /** @var EntityManager $em */
+        $em = self::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+        $count = (int) $em->getRepository(Security::class)
+            ->createQueryBuilder('s')
+            ->select('Count(s.id)')
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        $this->assertSame(1, $count);
+
+        $count = (int) $em->getRepository(Dinosaur::class)
+            ->createQueryBuilder('d')
+            ->select('Count(d.id)')
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        $this->assertSame(3, $count);
     }
 }
